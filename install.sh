4c369e8d21dd356872561565d38375a07ed27a60
9cb14c1ec0 <93002271+9cb14c1ec0@users.noreply.github.com>
2023-08-07 19:11:55 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 version=$(git -c 'versionsort.suffic=-' ls-remote --tags https://github.com/coder/code-server | grep -v 'rc.' | tail -n 1 | awk -F 'v' '{print $NF}')
 url="https://github.com/coder/code-server/releases/download/v$version/code-server_$version""_amd64.deb"
 wget $url
-sudo apt install "./code-server_$version""_amd64.deb"
+sudo apt install -y "./code-server_$version""_amd64.deb"
