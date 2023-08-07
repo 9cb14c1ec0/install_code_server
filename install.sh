aa53c4a9bfdc52271c30c979f42682ae09b4fc50
9cb14c1ec0 <93002271+9cb14c1ec0@users.noreply.github.com>
2023-08-07 19:01:58 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,4 @@
+version=$(git -c 'versionsort.suffic=-' ls-remote --tags https://github.com/coder/code-server | grep -v 'rc.' | tail -n 1 | awk -F 'v' '{print $NF}')
+url="https://github.com/coder/code-server/releases/download/v$version/code-server_$version""_amd64.deb"
+wget $url
+sudo apt install "./coder-server-$version""_amd64.deb"
